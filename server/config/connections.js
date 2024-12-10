1ba769f8b86e46759bd649621a0cfd9368b57e19
Gobind Singh <gobindbhari@gmail.com>
2024-12-10 12:27:49 +0530

    Update connections.js

--- server/config/connections.js
+++ server/config/connections.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const db = async () => {
     try {
         // await mongoose.connect('mongodb://localhost:27017/feedback-Review')
-        await mongoose.connect(`${process.env.MONGODB_URL}/test`)
+        await mongoose.connect(`${process.env.MONGODB_URL}`)
         console.log("Database connected successfully");
     } catch (error) {
         console.error("Error connecting to the database:", error);
